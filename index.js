3d21f1ac8e715a7983cd69a949abab4b2d19d8c3
Johan <ti.johan@ti7.com.br>
2022-09-19 17:29:34 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+var Continuar = "S";
+
+  let nomePiloto = prompt("Qual é o seu nome Piloto??");
+  let velocidadeNave = 0
+  let naveEspacial = prompt(`Qual nave o senhor gostaria de pilotar
+  1- Elemental(Velocidade maxima de: 200km/s);
+  2- X-Wing(velocidade maxima de: 325km/s);
+  3- A-Wing(velocidade maxima de: 150km/s);
+  4- Estelar Classe Executor(velocidade maxima de: 500km/s).
+`)
+  while (Continuar == "S") {
+  var seletor = prompt(`Painel de Controle:
+  1- Acelerar a nave;
+  2- Desacelerar a nave;
+  3- Trocar de nave;
+  4- Sair.`)
+
+  if(seletor == 1) {
+    
+  }
+}

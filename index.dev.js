fb17d04ccbee5307cdd01d3f8999d0e098f24182
Johan <ti.johan@ti7.com.br>
2022-09-20 15:58:36 -0300

    2 commit

--- index.dev.js
+++ index.dev.js
@@ -31,16 +31,24 @@ while (Continuar == "S") {
   }
 
   while (VerificadorPainel == "S") {
-    var seletor = prompt("Painel de Controle:\n  1- Acelerar a nave;\n  2- Desacelerar a nave;\n  3- Trocar de nave;\n  4- Sair.");
+    var seletor = prompt("Painel de Controle:\n  1- Acelerar a nave;\n  2- Desacelerar a nave;\n  3- Trocar de nave;\n  4- Sair.\n  Piloto: ".concat(nomePiloto, "\n  Velocidade atual da nave: ").concat(velocidadeNave, "km/s\n  Modelo da nave: ").concat(naveEspacial, "\n  "));
 
     if (seletor >= 5 || seletor <= 0) {
       alert("Digite somente um n\xFAmero entre 1 e 4");
-    } else if (seletor == 1) {
-      var acelerador = prompt("Digite quanto deseja acelerar\n    1- 5km/s;\n    2- 10km/s;\n    3- 20km/s;\n    4- 50km/s;\n    5- Sair.\n    Velocidade Maxima de: ".concat(VelocidadeMaxima, "km/s\n    Velocidade Atual: ").concat(velocidadeNave));
+    }
 
-      if (acelerador >= 5 || acelerador <= 0) {
+    while (seletor == 1) {
+      var acelerador = prompt("Digite quanto deseja acelerar\n    1- 5km/s;\n    2- 10km/s;\n    3- 20km/s;\n    4- 50km/s;\n    5- Voltar.\n    Velocidade Maxima de: ".concat(VelocidadeMaxima, "km/s\n    Velocidade Atual de: ").concat(velocidadeNave, "km/s\n    "));
+
+      if (acelerador >= 6 || acelerador <= 0) {
         Verificador1 = "S";
         alert("N\xFAmero inv\xE1lido");
+      } else if (acelerador == 5) {
+        alert("Voltando...");
+        seletor = "Easter Egg:)";
+      } else if (velocidadeNave >= VelocidadeMaxima) {
+        velocidadeNave = VelocidadeMaxima;
+        alert("Voc\xEA atingiu a velocidade m\xE1xima da nave");
       } else {
         var km = 0;
 
@@ -54,7 +62,74 @@ while (Continuar == "S") {
           km = 50;
         }
 
-        alert("Acelerando em:");
+        velocidadeNave += km; // velocidadeNave += km === velocidadeNave = velocidadeNave + km
+
+        alert("Acelerando em: ".concat(km, "km/s"));
+
+        if (velocidadeNave >= VelocidadeMaxima) {
+          velocidadeNave = VelocidadeMaxima;
+          alert("Voc\xEA atingiu a velocidade m\xE1xima da nave");
+        }
+      }
+    }
+
+    while (seletor == 2) {
+      var VelocidadeMinima = 0;
+      var desacelerador = prompt("Digite quanto deseja desacelerar\n    1- 5km/s;\n    2- 10km/s;\n    3- 20km/s;\n    4- 50km/s;\n    5- Voltar.\n    Velocidade Minima de: ".concat(VelocidadeMinima, "km/s\n    Velocidade Atual de: ").concat(velocidadeNave, "km/s\n    "));
+
+      if (desacelerador >= 6 || desacelerador <= 0) {
+        Verificador1 = "S";
+        alert("N\xFAmero inv\xE1lido");
+      } else if (desacelerador == 5) {
+        alert("Voltando...");
+        seletor = "Easter Egg:)";
+      } else if (velocidadeNave <= VelocidadeMinima) {
+        velocidadeNave = VelocidadeMinima;
+        alert("Voc\xEA atingiu a velocidade m\xEDnima da nave");
+      } else {
+        var km = 0;
+
+        if (desacelerador == 1) {
+          km = 5;
+        } else if (desacelerador == 2) {
+          km = 10;
+        } else if (desacelerador == 3) {
+          km = 20;
+        } else if (desacelerador == 4) {
+          km = 50;
+        }
+
+        velocidadeNave -= km; // velocidadeNave -= km === velocidadeNave = velocidadeNave - km
+
+        alert("Desacelerando em: ".concat(km, "km/s"));
+
+        if (velocidadeNave <= VelocidadeMinima) {
+          velocidadeNave = VelocidadeMinima;
+          alert("Voc\xEA atingiu a velocidade Minima da nave");
+        }
+      }
+    }
+
+    if (seletor == 3) {
+      var confirmar = confirm("Deseja confirmar a troca de nave?\n      Sua velocidade passara a ser 0");
+
+      if (confirmar == true) {
+        var naveEspacial = prompt("Escolha sua nova nave:\n        1- Elemental(Velocidade maxima de: 200km/s);\n    2- X-Wing(velocidade maxima de: 325km/s);\n    3- A-Wing(velocidade maxima de: 150km/s);\n    4- Estelar Classe Executor(velocidade maxima de: 500km/s).");
+        velocidadeNave = 0;
+      } else {
+        alert("Voltando...");
+      }
+    }
+
+    if (seletor == 4) {
+      alert("O nome do piloto \xE9: ".concat(nomePiloto, ". O modelo da nave \xE9: ").concat(naveEspacial, ". E a velocidade da nave \xE9 de: ").concat(velocidadeNave));
+      VerificadorPainel = "N";
+      Continuar = confirm("Deseja Reiniciar a opera\xE7\xE3o?");
+
+      if (Continuar == true) {
+        Continuar = "S";
+      } else {
+        alert("Agradecemos o feedback");
       }
     }
   }
--- index.js
+++ index.js
@@ -3,68 +3,146 @@ var Continuar = `S`;
 while (Continuar == `S`) {
   var nomePiloto = prompt("Qual é o seu nome Piloto??");
   var velocidadeNave = 0;
-  var VerificadorNave = `S`
-  while(VerificadorNave == `S`){
+  var VerificadorNave = `S`;
+  while (VerificadorNave == `S`) {
     var naveEspacial = prompt(`Qual nave o senhor gostaria de pilotar
     1- Elemental(Velocidade maxima de: 200km/s);
     2- X-Wing(velocidade maxima de: 325km/s);
     3- A-Wing(velocidade maxima de: 150km/s);
     4- Estelar Classe Executor(velocidade maxima de: 500km/s).
   `);
-  if(naveEspacial >= 5 || naveEspacial <= 0 || isNaN(naveEspacial)){
-    alert(`Digite um número válido`)
-  } else {
-    VerificadorNave = `N`
-  }
-  }
-  var VerificadorPainel = `S`
-  var VelocidadeMaxima = 0
-
-    if(naveEspacial == 1) {
-      VelocidadeMaxima = 200
-    } else if (naveEspacial == 2) {
-      VelocidadeMaxima = 325
-    } else if (naveEspacial == 3) {
-      VelocidadeMaxima = 150
-    } else if (naveEspacial == 4) {
-      VelocidadeMaxima = 500
+    if (naveEspacial >= 5 || naveEspacial <= 0 || isNaN(naveEspacial)) {
+      alert(`Digite um número válido`);
+    } else {
+      VerificadorNave = `N`;
     }
-      while (VerificadorPainel == `S`) {
-        var seletor = prompt(`Painel de Controle:
+  }
+  var VerificadorPainel = `S`;
+  var VelocidadeMaxima = 0;
+
+  if (naveEspacial == 1) {
+    VelocidadeMaxima = 200;
+  } else if (naveEspacial == 2) {
+    VelocidadeMaxima = 325;
+  } else if (naveEspacial == 3) {
+    VelocidadeMaxima = 150;
+  } else if (naveEspacial == 4) {
+    VelocidadeMaxima = 500;
+  }
+  while (VerificadorPainel == `S`) {
+    var seletor = prompt(`Painel de Controle:
   1- Acelerar a nave;
   2- Desacelerar a nave;
   3- Trocar de nave;
-  4- Sair.`);
+  4- Sair.
+  Piloto: ${nomePiloto}
+  Velocidade atual da nave: ${velocidadeNave}km/s
+  Modelo da nave: ${naveEspacial}
+  `);
 
-  if (seletor >= 5 || seletor <= 0) {
-    alert(`Digite somente um número entre 1 e 4`);
-  } 
-  else if (seletor == 1) {
-    var acelerador = prompt(`Digite quanto deseja acelerar
+    if (seletor >= 5 || seletor <= 0) {
+      alert(`Digite somente um número entre 1 e 4`);
+    }
+    while (seletor == 1) {
+      var acelerador = prompt(`Digite quanto deseja acelerar
     1- 5km/s;
     2- 10km/s;
     3- 20km/s;
     4- 50km/s;
-    5- Sair.
+    5- Voltar.
     Velocidade Maxima de: ${VelocidadeMaxima}km/s
-    Velocidade Atual: ${velocidadeNave}`)
-    if(acelerador >= 5 || acelerador <= 0) {
-      Verificador1 = `S`
-      alert(`Número inválido`)
-    } else {
-      var km = 0
-      if(acelerador == 1) {
-        km = 5
-      } else if (acelerador == 2) {
-        km = 10
-      } else if (acelerador == 3) {
-        km = 20
-      } else if (acelerador == 4) {
-        km = 50
+    Velocidade Atual de: ${velocidadeNave}km/s
+    `);
+      if (acelerador >= 6 || acelerador <= 0) {
+        Verificador1 = `S`;
+        alert(`Número inválido`);
+      } else if (acelerador == 5) {
+        alert(`Voltando...`);
+        seletor = `Easter Egg:)`;
+      } else if (velocidadeNave >= VelocidadeMaxima) {
+        velocidadeNave = VelocidadeMaxima;
+        alert(`Você atingiu a velocidade máxima da nave`);
+      } else {
+        var km = 0;
+        if (acelerador == 1) {
+          km = 5;
+        } else if (acelerador == 2) {
+          km = 10;
+        } else if (acelerador == 3) {
+          km = 20;
+        } else if (acelerador == 4) {
+          km = 50;
+        }
+        velocidadeNave += km; // velocidadeNave += km === velocidadeNave = velocidadeNave + km
+        alert(`Acelerando em: ${km}km/s`);
+        if (velocidadeNave >= VelocidadeMaxima) {
+          velocidadeNave = VelocidadeMaxima;
+          alert(`Você atingiu a velocidade máxima da nave`);
+        }
       }
-      alert(`Acelerando em:`)
+    }
+    while (seletor == 2) {
+      var VelocidadeMinima = 0;
+      var desacelerador = prompt(`Digite quanto deseja desacelerar
+    1- 5km/s;
+    2- 10km/s;
+    3- 20km/s;
+    4- 50km/s;
+    5- Voltar.
+    Velocidade Minima de: ${VelocidadeMinima}km/s
+    Velocidade Atual de: ${velocidadeNave}km/s
+    `);
+      if (desacelerador >= 6 || desacelerador <= 0) {
+        Verificador1 = `S`;
+        alert(`Número inválido`);
+      } else if (desacelerador == 5) {
+        alert(`Voltando...`);
+        seletor = `Easter Egg:)`;
+      } else if (velocidadeNave <= VelocidadeMinima) {
+        velocidadeNave = VelocidadeMinima;
+        alert(`Você atingiu a velocidade mínima da nave`);
+      } else {
+        var km = 0;
+        if (desacelerador == 1) {
+          km = 5;
+        } else if (desacelerador == 2) {
+          km = 10;
+        } else if (desacelerador == 3) {
+          km = 20;
+        } else if (desacelerador == 4) {
+          km = 50;
+        }
+        velocidadeNave -= km; // velocidadeNave -= km === velocidadeNave = velocidadeNave - km
+        alert(`Desacelerando em: ${km}km/s`);
+        if (velocidadeNave <= VelocidadeMinima) {
+          velocidadeNave = VelocidadeMinima;
+          alert(`Você atingiu a velocidade Minima da nave`);
+        }
+      }
+    }
+    if (seletor == 3) {
+      var confirmar = confirm(`Deseja confirmar a troca de nave?
+      Sua velocidade passara a ser 0`)
+      if (confirmar == true) {
+        var naveEspacial = prompt(`Escolha sua nova nave:
+        1- Elemental(Velocidade maxima de: 200km/s);
+    2- X-Wing(velocidade maxima de: 325km/s);
+    3- A-Wing(velocidade maxima de: 150km/s);
+    4- Estelar Classe Executor(velocidade maxima de: 500km/s).`)
+    velocidadeNave = 0
+      } else {
+        alert(`Voltando...`)
+      }
+    }
+    if (seletor == 4) {
+      alert(`O nome do piloto é: ${nomePiloto}. O modelo da nave é: ${naveEspacial}. E a velocidade da nave é de: ${velocidadeNave}`)
+      VerificadorPainel = `N`;
+      Continuar = confirm(`Deseja Reiniciar a operação?`);
+      if(Continuar == true) {
+        Continuar = `S`
+      }else {
+        alert(`Agradecemos o feedback`)
+      }
     }
   }
-      }
-    
 }
